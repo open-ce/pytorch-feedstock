0d972290c4ec15c805b2a103170cb83fd5574ae0
Bryant Nelson <bryant.nelson@ibm.com>
2020-09-08 18:32:56 -0500

    Initial copy.

---
+++ scripts/pytorch-deactivate-env-vars.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# *****************************************************************
+#
+# Licensed Materials - Property of IBM
+#
+# (C) Copyright IBM Corp. 2019, 2020. All Rights Reserved.
+#
+# US Government Users Restricted Rights - Use, duplication or
+# disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
+#
+# *****************************************************************
+
+unset OMP_NUM_THREADS
+unset TORCH_CUDA_ARCH_LIST
+
