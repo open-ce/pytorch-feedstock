c6394d80b9bfd7a356a32c14236f91c9522763df
Deepali <70963368+cdeepali@users.noreply.github.com>
2021-05-25 13:08:39 +0530

    reorganize build script code (#55)

--- recipe/build.sh
+++ recipe/build.sh
@@ -32,31 +32,8 @@ export CXXFLAGS="${CXXFLAGS} -D__STDC_FORMAT_MACROS"
 export LDFLAGS="$(echo ${LDFLAGS} | sed -e 's/-Wl\,--as-needed//')"
 export LDFLAGS="${LDFLAGS} -Wl,-rpath-link,${PREFIX}/lib"
 
-if [[ $build_type == "cpu" ]]
-then
-  # Disable GPU-related libraries
-  export USE_CUDA=0
-  export USE_CUDNN=0
-  export USE_TENSORRT=0
-
-elif [[ $build_type == "cuda" ]]
-then
-  # Enable GPU-related libraries
-  export USE_CUDA=1
-  export USE_CUDNN=1
-  if [[ $PY_VER < 3.8 ]]; then
-    export USE_TENSORRT=1
-  else
-    export USE_TENSORRT=0
-  fi
-else
-    echo "build_type was set to '$build_type', an invalid value."
-    echo "build_type must be set to either 'cpu' or 'cuda'."
-    exit 1
-fi
-
-export PYTORCH_BUILD_VERSION=${PKG_VERSION}
-export PYTORCH_BUILD_NUMBER=0
+# needed so cmake can find the conda version of librt.so and other libraries and headers
+export CMAKE_PREFIX_PATH="${BUILD_PREFIX}/${HOST}/sysroot/usr/;${PREFIX}"
 
 # select OpenBLAS for BLAS
 export BLAS=OpenBLAS
@@ -79,51 +56,67 @@ export USE_MPI=0
 
 export BUILD_CUSTOM_PROTOBUF=OFF
 
-# needed so cmake can find the conda version of librt.so and other libraries and headers
-export CMAKE_PREFIX_PATH="${BUILD_PREFIX}/${HOST}/sysroot/usr/;${PREFIX}"
+export PYTORCH_BUILD_VERSION=${PKG_VERSION}
+export PYTORCH_BUILD_NUMBER=0
 
-if [[ "$USE_CUDA" == 1 ]]; then
-    export TORCH_CUDA_ARCH_LIST="3.7;6.0;7.0;7.5"
-    if [[ $CUDA_VERSION == '11' ]]; then
-        export TORCH_CUDA_ARCH_LIST="$TORCH_CUDA_ARCH_LIST;8.0"
-    fi
-    export TORCH_NVCC_FLAGS="-Xfatbin -compress-all"
+function apply_trt_patches() {
+  # update onnx-tensorrt submodule
+  ARCH=`uname -p`
+  cd third_party/onnx-tensorrt
+  if [[ $cudatoolkit == '11.0' ]]; then
+      git checkout eb559b6cdd1ec2169d64c0112fab9b564d8d503b   #corresponds to TRT 7.2
+      cd ../..
+      # apply fix for GLIBC
+      if [[ "${ARCH}" == 'x86_64' ]]; then
+          git apply ${RECIPE_DIR}/0300-onnx-tensorrt-Fix-for-GLIBC_2.14_TRT72.patch
+      fi
+  elif [[ $cudatoolkit == '10.2' ]]; then
+      git checkout 84b5be1d6fc03564f2c0dba85a2ee75bad242c2e   #corresponds to TRT 7.0
+      cd ../..
+      if [[ "${ARCH}" == 'x86_64' ]]; then
+          # apply fix for GLIBC
+          git apply ${RECIPE_DIR}/0300-onnx-tensorrt-Fix-for-GLIBC_2.14_TRT70.patch
+      fi
+  fi
+}
 
-    # Create symlinks of cublas headers into CONDA_PREFIX
-    mkdir -p $CONDA_PREFIX/include
-    find /usr/include -name cublas*.h -exec ln -s "{}" "$CONDA_PREFIX/include/" ';'
-    export CXXFLAGS="${CXXFLAGS} -I${PREFIX}/include -I${CUDA_HOME}/include -I${CONDA_PREFIX}/include"
+if [[ $build_type == "cpu" ]]
+then
+  # Disable GPU-related libraries
+  export USE_CUDA=0
+  export USE_CUDNN=0
+  export USE_TENSORRT=0
+
+elif [[ $build_type == "cuda" ]]
+then
+  # Enable GPU-related libraries
+  export USE_CUDA=1
+  export USE_CUDNN=1
+
+  export TORCH_CUDA_ARCH_LIST="3.7;6.0;7.0;7.5"
+  if [[ $CUDA_VERSION == '11' ]]; then
+     export TORCH_CUDA_ARCH_LIST="$TORCH_CUDA_ARCH_LIST;8.0"
+  fi
+  export TORCH_NVCC_FLAGS="-Xfatbin -compress-all"
+
+  # Create symlinks of cublas headers into CONDA_PREFIX
+  mkdir -p $CONDA_PREFIX/include
+  find /usr/include -name cublas*.h -exec ln -s "{}" "$CONDA_PREFIX/include/" ';'
+  export CXXFLAGS="${CXXFLAGS} -I${PREFIX}/include -I${CUDA_HOME}/include -I${CONDA_PREFIX}/include"
+
+  if [[ $PY_VER < 3.8 ]]
+  then
+    export USE_TENSORRT=1
+    apply_trt_patches
+  else
+    export USE_TENSORRT=0
+  fi
 fi
 
 # use v1.6.0 for onnx submodule
 cd third_party/onnx
 git checkout 553df22c67bee5f0fe6599cff60f1afc6748c635
 cd ../..
-
-if [[ $PY_VER < 3.8 ]] && [[ $cudatoolkit ]];
-then
-    # update onnx-tensorrt submodule
-    ARCH=`uname -p`
-    cd third_party/onnx-tensorrt
-    if [[ $cudatoolkit == '11.0' ]]; then
-        git checkout eb559b6cdd1ec2169d64c0112fab9b564d8d503b   #corresponds to TRT 7.2
-        cd ../..
-        # apply fix for GLIBC
-        if [[ "${ARCH}" == 'x86_64' ]]; then
-            git apply ${RECIPE_DIR}/0300-onnx-tensorrt-Fix-for-GLIBC_2.14_TRT72.patch
-        fi
-    elif [[ $cudatoolkit == '10.2' ]]; then
-        git checkout 84b5be1d6fc03564f2c0dba85a2ee75bad242c2e   #corresponds to TRT 7.0
-        cd ../..
-        if [[ "${ARCH}" == 'x86_64' ]]; then
-            # apply fix for GLIBC
-            git apply ${RECIPE_DIR}/0300-onnx-tensorrt-Fix-for-GLIBC_2.14_TRT70.patch
-        fi
-    else
-        # fail safe to get back to /
-        cd ../..
-    fi
-fi
 
 # install
 MAX_JOBS=${CPU_COUNT} python setup.py install
